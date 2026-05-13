49a52a70e248d6429327e9cf806a06fdb8993645
Tapia Solano Daniel de Jesus <tasd920515@gmail.com>
2026-05-12 23:52:04 -0600

    AutomataA Funcionando
    El automataA ya hace las transiciones como se debe, solo hay un pequeño desfase en el led de estado

--- EjemplosYPruebas/AutomataA/src/AutomataA.v
+++ EjemplosYPruebas/AutomataA/src/AutomataA.v
@@ -7,7 +7,7 @@ module AutomataA (
     output [6:0] display
 );
 
-wire Q2, Q1, Q0, Q2n, Q1n, Qn0;
+wire Q2, Q1, Q0;
 wire D2, D1, D0;  
 wire [4:0] entradaDeco; // Se va ha tratar de completar los bits, ya que el creado tiene 5 bits
 
@@ -36,38 +36,52 @@ FlipFlopD FFD2(
     .D(D2),
     .clk(led),    
     .clr(clr),
-    .Q(Q2),
-    .Qn(Q2n)
+    .q(Q2)
 );
 
 FlipFlopD FFD1(
     .D(D1),
     .clk(led),
     .clr(clr),
-    .Q(Q1),
-    .Qn(Q1n)
+    .q(Q1) 
 );
 
 FlipFlopD FFD0 (
     .D(D0),
     .clk(led),
     .clr(clr),
-    .Q(Q0),
-    .Qn(Qn0)
+    .q(Q0)
 );
 ///////////////////Fin instancia FF/////////////////////////
 ////////////////////////////////////////////////////////////
 
-assign D2 = (~X & Q2n & Q1 & Q0) | (~X & Q2 & Q1n & Qn0) | (X & Q2n & Q1 & Q0) | (X & Q2 & Q1n & Q0);
-assign D1 = (Q2 & Q1 & Qn0) | (X & Q2n & Q1n & Q0) | (X & Q2 & Q1n & Qn0);
-assign D0 = (~X & Q2n & Q1n & Q0) | (~X & Q2n & Q1 & Qn0) | (~X & Q2 & Q1n & Qn0) | (X & Q2n & Q1n & Qn0);  
+/*
+//Ecuaciones con errores, corregir
+assign D2 = (~X & ~Q2 & Q1 & Q0) | (~X & Q2 & ~Q1 & ~Q0) | (X & ~Q2 & Q1 & Q0) | (X & Q2 & ~Q1 & Q0);
+assign D1 = (Q2 & Q1 & ~Q0) | (X & ~Q2 & ~Q1 & Q0) | (X & Q2 & ~Q1 & ~Q0);
+assign D0 = (~X & ~Q2 & ~Q1 & Q0) | (~X & ~Q2 & Q1 & ~Q0) | (~X & Q2 & ~Q1 & ~Q0) | (X & ~Q2 & ~Q1 & ~Q0);  
+*/
 
+// ECUACIONES CORREGIDAS 
+assign D2 = (~X & ~Q2 & ~Q1 & Q0) |  
+            (~X & Q2 & ~Q1 & ~Q0) |  
+            (X & ~Q2 & Q1 & Q0) |    
+            (X & Q2 & ~Q1 & Q0);     
+
+assign D1 = (X & ~Q2 & ~Q1 & Q0) |  
+            (~Q2 & Q1 & ~Q0) |       
+            (X & Q2 & ~Q1 & ~Q0);    
+
+assign D0 = (X & ~Q2 & ~Q1 & ~Q0) | 
+            (~X & ~Q2 & ~Q1 & Q0) |  
+            (~X & ~Q2 & Q1 & ~Q0) |  
+            (~X & Q2 & ~Q1 & ~Q0);   
 
 always @(posedge led or negedge clr) begin
     if (!clr) begin
         salida1 <= 0;
     end else begin
-        salida1 = (~Q2 & Q1 & Q0) | (Q2 & ~Q1 & ~Q0);
+        salida1 <= (~Q2 & Q1 & Q0) | (Q2 & ~Q1 & ~Q0);
     end
 end
 
